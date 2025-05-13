70ef330bcc3886ff49148a7038c7184bcbefc3d4
CeliaCampanha <celiacampanha@hotmail.com>
2025-05-13 16:54:30 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gs__0026",
+      "name": "GS__0026",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.4805361918039122,
+        "pitch": 0.06209191545650228,
+        "fov": 1.3305650248204532
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.651078177545779,
+          "pitch": -0.13169732316286797,
+          "rotation": 0,
+          "target": "2-gs__0028"
+        },
+        {
+          "yaw": 1.9670492089277278,
+          "pitch": -0.22395070166326292,
+          "rotation": 0,
+          "target": "1-gs__0027"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gs__0027",
+      "name": "GS__0027",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6597992514695852,
+          "pitch": 0.033269598858433724,
+          "rotation": 0,
+          "target": "2-gs__0028"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gs__0028",
+      "name": "GS__0028",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
